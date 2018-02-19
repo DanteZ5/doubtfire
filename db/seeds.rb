c3327f4c07a6caa2902810f3f83a11610693a6be
Maximilien Rufin <maximilien.rufin@gmail.com>
2018-02-19 18:20:37 +0100

    grandma seed is up and running

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+Grandma.destroy_all
+User.destroy_all
+
+users_attributes = [
+{ email: 'raymonde@laposte.fr', password: 'kiki38' },
+{ email: 'françoise@laposte.fr', password: 'michael44' },
+{ email: 'jacqueline@laposte.fr', password: 'jerome56' },
+{ email: 'yvette@laposte.fr', password: 'fabrice' }
+]
+users = User.create!(users_attributes)
+
+p users.second.id
+
+grandma_attributes = [
+{ user_id: users.second.id, price: 13, cooking: true },
+{ user_id: users.third.id, price: 15, knitting: true }
+]
+grandmas = Grandma.create!(grandma_attributes)
+
+
