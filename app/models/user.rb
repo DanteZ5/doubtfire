7c3b9e89c65e336e9604f83f5996dd1205117538
Nass <mezouar.nass@gmail.com>
2018-02-19 17:26:05 +0100

    add user_type to user table and add collection to signup view

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :users
+
+  USER_TYPES = ["Grandma", "Customer"]
 end
