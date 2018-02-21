30e03e3324257c2e1f5e98ab783df9ca34145722
Nass <mezouar.nass@gmail.com>
2018-02-21 11:22:03 +0100

    add upload photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_type, :first_name, :last_name, :age, :avatar, :username, :address, :description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_type, :first_name, :last_name, :age, :avatar, :username, :address, :description, :photo])
     # For additional in app/views/devise/registrations/edit.html.erb
     # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
